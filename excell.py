59627522d93bedb0ea3669a98a7226d057b61fb5
SwayamBadhe <swayampbadhe@gmail.com>
2023-10-08 02:19:26 +0530

    added calculation till Plate Sizes

--- excell.py
+++ excell.py
@@ -2,6 +2,7 @@ import numpy as np
 import pandas as pd
 import methods
 
+Zp_case3 = 0
 
 # Input data from the user
 def inputDefaultValues():
@@ -19,6 +20,14 @@ def input_bending_moment():
     max_shear_force_kN = float(input("Enter Maximum Shear Force (in kN): "))
 
     return (max_moment_mid_span_kNm, max_moment_support_kNm, max_shear_force_kN)  
+
+def input_plate_sizes():
+    depth_mm = float(input("Enter Plate Depth (in mm): "))
+    flange_width_mm = float(input("Enter Plate Flange Depth (in mm): "))
+    flange_thickness_mm = float(input("Enter Plate Flange Thickness (in mm): "))
+    web_thickness_mm = float(input("Enter Plate Web Thickness (in mm): "))
+
+    return (depth_mm, flange_width_mm, flange_thickness_mm, web_thickness_mm)
 
 
 # Function to write data to CSV
@@ -46,18 +55,33 @@ while (True):
                                 (Type - Bending Moment Input)
                             3. Plastic Section Modulus Required,Zp
                                 (Type - Plastic Section)
+                            4. Selection of suitable Plate Sizes
+                                (Type - Suitable Plate Sizes)
                             Enter your choice: 
                         ''')
 
-    def switch(calculation_type):        
+    def switch(calculation_type): 
+        global Zp_case3     
 
-        if (calculation_type == "Beam L" or calculation_type == "1"):
-            initial_values = methods.getInitialValues()
-            unsupported_length_m = initial_values["unsupported_length_m"]
-            yield_strength_MPa = initial_values["yield_strength_MPa"]
-            partial_safety_factor = initial_values["partial_safety_factor"]
-            modulus_of_elasticity_N_per_mm2 = initial_values["modulus_of_elasticity_N_per_mm2"]
-            bearing_support_width_mm = initial_values["bearing_support_width_mm"]
+        initial_values = methods.getInitialValues()
+        unsupported_length_m = initial_values["unsupported_length_m"]
+        yield_strength_MPa = initial_values["yield_strength_MPa"]
+        partial_safety_factor = initial_values["partial_safety_factor"]
+        modulus_of_elasticity_N_per_mm2 = initial_values["modulus_of_elasticity_N_per_mm2"]
+        bearing_support_width_mm = initial_values["bearing_support_width_mm"]  
+
+        bending_moment_inital_values = methods.getLocalValues2()
+        max_moment_mid_span_kNm = bending_moment_inital_values["max_moment_mid_span_kNm"]
+        max_moment_support_kNm = bending_moment_inital_values["max_moment_support_kNm"]
+        max_shear_force_kN = bending_moment_inital_values["max_shear_force_kN"]
+
+        plate_size_initial_values = methods.getLocalValues4()
+        depth_mm = plate_size_initial_values["depth_mm"]
+        flange_width_mm = plate_size_initial_values["flange_width_mm"]
+        flange_thickness_mm = plate_size_initial_values["flange_thickness_mm"]
+        web_thickness_mm = plate_size_initial_values["web_thickness_mm"]
+
+        if (calculation_type == "Beam L" or calculation_type == "1"):            
             L = methods.calculate_effective_length(unsupported_length_m, bearing_support_width_mm)
             print(f"Effective length of Beam L (c/c of column support): {L} meters")
 
@@ -75,7 +99,11 @@ while (True):
             write_to_csv('results.csv', data)
 
         elif (calculation_type == "Bending Moment" or calculation_type == "2"):
-            max_moment_mid_span_kNm, max_moment_support_kNm, max_shear_force_kN = input_bending_moment()
+            bending_moment_input_bool = input("Do you want to use preset values?: ")
+            if (bending_moment_input_bool == "n" or bending_moment_input_bool == "N"):
+                max_moment_mid_span_kNm, max_moment_support_kNm, max_shear_force_kN = input_bending_moment()
+                methods.setLocalValues2(max_moment_mid_span_kNm, max_moment_support_kNm, max_shear_force_kN)
+            
             methods.setLocalValues3(max_moment_mid_span_kNm, max_moment_support_kNm)
             
 
@@ -89,24 +117,69 @@ while (True):
             append_to_csv('results.csv', data)
         
         elif (calculation_type == "Plastic Section" or calculation_type == "3"):
-            max_moment_mid_span_kNm, max_moment_support_kNm = methods.getLocalValues3()
             max_moment_kNm = max(max_moment_mid_span_kNm, max_moment_support_kNm)
 
             # Calculate Zp
-            Zp = methods.ZpFunc(max_moment_kNm, partial_safety_factor, yield_strength_MPa)
+            Zp_case3 =  Zp = methods.ZpFunc(max_moment_kNm, partial_safety_factor, yield_strength_MPa)
 
+            print(f"Max Moment of mid and support Moment: {max_moment_kNm} KNm")
             print(f"Plastic Section Modulus Required (Zp): {Zp} mm^3")
+
 
             # Prepare data for writing to CSV
             data = [
                 ("Max Moment (kNm): ", max_moment_kNm),
                 ("Partial Safety Factor: ", partial_safety_factor),
                 ("Yield Strength of Steel (MPa): ", yield_strength_MPa),
+                ("MAx Moment of mid nad support Moment: ", max_moment_kNm),
                 ("Plastic Section Modulus Required (mm^3): ", Zp)
             ]
 
             # Append data to CSV file
             append_to_csv('results.csv', data)
+
+        elif (calculation_type == "Suitable Plate Sizes" or calculation_type == "4"):
+            plate_size_input_bool = input("Do you want to use preset values?: ")
+            if (plate_size_input_bool == "n" or plate_size_input_bool == "N"):
+                depth_mm, flange_width_mm, flange_thickness_mm, web_thickness_mm = input_plate_sizes()
+                methods.setLocalValues4(depth_mm, flange_width_mm, flange_thickness_mm, web_thickness_mm)
+            
+            plate_properties = methods.calculate_plate_properties(depth_mm, flange_width_mm, flange_thickness_mm, web_thickness_mm, yield_strength_MPa)
+
+            web_depth_mm = plate_properties["web_depth_mm"]
+            Ix = plate_properties["Ix"]
+            Iy = plate_properties["Iy"]
+            epsilon = plate_properties["epsilon"]
+            Ze = plate_properties["Ze"]
+            Zp = plate_properties["Zp"]
+
+            print(f"Web Depth: {web_depth_mm} mm")
+            print(f"Moment of Inertia about major axis (Ix): {Ix} mm^4")
+            print(f"Moment of Inertia about weak or minor axis (Iy): {Iy} mm^4")
+            print(f"Epsilon: {epsilon}")
+            print(f"Elastic Section Modulus (Ze): {Ze} mm^3")
+            print(f"Plastic Section Modulus (Zp): {Zp} mm^3")    
+
+            if (Zp >= Zp_case3):
+                print("Provided section is ok")
+                msg = "Provided section is ok"
+            else:
+                print("Revise the section")
+                msg = "Revise the section"
+
+            data = [
+                ("Web Depth (mm): ", web_depth_mm),
+                ("Moment of Inertia about major axis (Ix) (mm^4): ", Ix),
+                ("Moment of Inertia about weak or minor axis (Iy) (mm^4): ", Iy),
+                ("Epsilon: ", epsilon),
+                ("Elastic Section Modulus (Ze) (mm^3): ", Ze),
+                ("Plastic Section Modulus (Zp) (mm^3): ", Zp),
+                ("Message: ", msg)
+            ]
+
+            # Append data to CSV file
+            append_to_csv('results.csv', data)
+
 
     switch(calculation_type)
 
--- methods.py
+++ methods.py
@@ -1,11 +1,18 @@
 # ------------------------------global variable declaration-----------------------------------
-g_max_moment_mid_span_kNm = 0
-g_max_moment_support_kNm = 0
 g_unsupported_length_m = 20
 g_yield_strength_MPa = 250
 g_partial_safety_factor = 1.1
 g_modulus_of_elasticity_N_per_mm2 = 200000
 g_bearing_support_width_mm = 200
+
+g_max_moment_mid_span_kNm = 7031
+g_max_moment_support_kNm = 0
+g_max_shear_force_kN = 1012.50
+
+g_depth_mm = 1700
+g_flange_width_mm = 400
+g_flange_thickness_mm = 30
+g_web_thickness_mm = 34
 
 # Define calculation functions
 def calculate_effective_length(unsupported_length_m, bearing_support_width_mm):
@@ -16,6 +23,35 @@ def ZpFunc(max_moment_kNm, partial_safety_factor, yield_strength_MPa):
     Zp = (max_moment_kNm * 1000000 * partial_safety_factor) / yield_strength_MPa
     return Zp
 
+def calculate_plate_properties(depth_mm, flange_depth_mm, flange_thickness_mm, web_thickness_mm, yield_strength_MPa):
+    # Calculate web depth
+    web_depth_mm = depth_mm - 2 * flange_thickness_mm
+
+    # Calculate Moment of Inertia about major axis (Ix)
+    Ix = (flange_depth_mm * depth_mm ** 3 / 12) - (((flange_depth_mm - web_thickness_mm) * (depth_mm - 2 * flange_thickness_mm) ** 3) / 12)
+
+    # Calculate Moment of Inertia about weak or minor axis (Iy)
+    Iy = ((2 * flange_thickness_mm * flange_depth_mm ** 3) / 12) + (((depth_mm - 2 * flange_thickness_mm) * web_thickness_mm ** 3) / 12)
+
+    # Calculate epsilon
+    epsilon = (250 / yield_strength_MPa) ** 0.5
+
+    # Calculate Elastic Section Modulus (Ze)
+    Ze = Ix / (depth_mm / 2)
+
+    # Calculate Plastic Section Modulus (Zp)
+    Zp = Ze * 1.135
+
+    return {
+        "web_depth_mm": web_depth_mm,
+        "Ix": Ix,
+        "Iy": Iy,
+        "epsilon": epsilon,
+        "Ze": Ze,
+        "Zp": Zp
+    }
+
+
 # -------get and set function for intial values (case 1)
 def getInitialValues():
     return {
@@ -25,7 +61,6 @@ def getInitialValues():
         "partial_safety_factor": g_partial_safety_factor,
         "modulus_of_elasticity_N_per_mm2": g_modulus_of_elasticity_N_per_mm2
     }
-
 
 def setInitialValues(unsupported_length_m, yield_strength_MPa, partial_safety_factor, modulus_of_elasticity_N_per_mm2, bearing_support_width_mm):
     global g_unsupported_length_m
@@ -39,9 +74,27 @@ def setInitialValues(unsupported_length_m, yield_strength_MPa, partial_safety_fa
     g_partial_safety_factor = partial_safety_factor
     g_modulus_of_elasticity_N_per_mm2 = modulus_of_elasticity_N_per_mm2
 
+# -------get and set function for Bending Moment (case 2)
+def getLocalValues2():
+    return {
+        "max_moment_mid_span_kNm": g_max_moment_mid_span_kNm,
+        "max_moment_support_kNm": g_max_moment_support_kNm,
+        "max_shear_force_kN": g_max_shear_force_kN
+    }
+
+def setLocalValues2(max_moment_mid_span_kNm, max_moment_support_kNm, max_shear_force_kN):
+    global g_max_moment_mid_span_kNm, g_max_moment_support_kNm, g_max_shear_force_kN
+    g_max_moment_mid_span_kNm = max_moment_mid_span_kNm
+    g_max_moment_support_kNm = max_moment_support_kNm
+    g_max_shear_force_kN = max_shear_force_kN
+
+
 # -------get and set function for plastic section modulus (case 3)
-def getLocalValues3(): 
-    return (g_max_moment_mid_span_kNm, g_max_moment_support_kNm)
+# def getLocalValues3(): 
+#     return {
+#         "max_moment_mid_span_kNm": g_max_moment_mid_span_kNm,
+#         "max_moment_support_kNm": g_max_moment_support_kNm,
+#     }
 
 def setLocalValues3(max_moment_mid_span_kNm, max_moment_support_kNm):
     global g_max_moment_mid_span_kNm
@@ -49,3 +102,18 @@ def setLocalValues3(max_moment_mid_span_kNm, max_moment_support_kNm):
     g_max_moment_mid_span_kNm = max_moment_mid_span_kNm
     g_max_moment_support_kNm = max_moment_support_kNm
     
+# -------get and set function for suitable Plate Sizes (case 4)
+def getLocalValues4():
+    return {
+        "depth_mm": g_depth_mm,
+        "flange_width_mm": g_flange_width_mm,
+        "flange_thickness_mm": g_flange_thickness_mm,
+        "web_thickness_mm": g_web_thickness_mm
+    }
+
+def setLocalValues4(depth_mm, flange_width_mm, flange_thickness_mm, web_thickness_mm):
+    global g_depth_mm, g_flange_width_mm, g_flange_thickness_mm, g_web_thickness_mm
+    g_depth_mm = depth_mm
+    g_flange_width_mm = flange_width_mm
+    g_flange_thickness_mm = flange_thickness_mm
+    g_web_thickness_mm = web_thickness_mm
